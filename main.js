b3a55a59f332de316a4f912b13a69f25202d77c2
TriiNoxYs <triinoxys@gmail.com>
2016-08-12 21:56:21 +0200

    change circle in main.js

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 window.onload = function onLoad() {
-    var circle = new ProgressBar.Circle('#progress', {
-        color: '#FCB03C',
-        duration: 3000,
-        easing: 'easeInOut'
-    });
+	var bar = new ProgressBar.Circle(container, {
+	  color: '#aaa',
+	  strokeWidth: 4,
+	  trailWidth: 1,
+	  easing: 'easeInOut',
+	  duration: 1400,
+	  text: {
+	    autoStyleContainer: false
+	  },
+	  from: { color: '#aaa', width: 1 },
+	  to: { color: '#56177D', width: 4 },
+	  step: function(state, circle) {
+	    circle.path.setAttribute('stroke', state.color);
+	    circle.path.setAttribute('stroke-width', state.width);
 
-    circle.animate(1);
+	    var value = Math.round(circle.value() * 100);
+	    if (value === 0) {
+	      circle.setText('');
+	    } else {
+	      circle.setText(value);
+	    }
+
+	  }
+	});
+	bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+	bar.text.style.fontSize = '2rem';
+
+	bar.animate(0.5);
 };
