7eb31f7a924144bac4c612322cb55affce3ac674
TriiNoxYs <triinoxys@gmail.com>
2016-08-13 02:02:01 +0200

    Add JS func to draw circle easily

---
+++ js/circle.js
@@ -0,0 +1,34 @@
+function circle(val) {
+    window.onload = function onLoad() {
+		var bar = new ProgressBar.Circle('#progress', {
+			color: '#aaa',
+			strokeWidth: 4,
+			trailWidth: 1,
+			easing: 'easeInOut',
+			duration: 1400,
+			text: {
+			autoStyleContainer: true
+			},
+			from: { 
+			color: '#aaa',
+			width: 1
+			},
+			to: { 
+			color: '#56177D', 
+			width: 4
+			},
+			step: function(state, circle) {
+				circle.path.setAttribute('stroke', state.color);
+				circle.path.setAttribute('stroke-width', state.width);
+
+				var value = Math.round(circle.value() * 100);
+				if(value === 0) circle.setText('');
+				else circle.setText(value +'%');
+			}
+		});
+		bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+		bar.text.style.fontSize = '2rem';
+
+		bar.animate(val);
+	};
+}
